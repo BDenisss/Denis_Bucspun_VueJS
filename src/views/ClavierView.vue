3fb039bbe3e1200eb2a351a3557d6e9702b65639
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2023-01-22 23:01:27 +0100

    :lipstick: Improve style

--- src/views/ClavierView.vue
+++ src/views/ClavierView.vue
@@ -1,34 +1,37 @@
 <template>
   <h1>Clavier</h1>
-  <div class="phone">
-    <form>
-      <div class="container_input">
-        <div class="input">
-          <input type="text" v-model="numeros">
+  <div class="phone_container">
+    <div class="phone">
+      <form>
+        <div class="container_input">
+          <div class="input">
+            <input type="text" v-model="numeros">
+          </div>
+          <div v-for="contacts in contact" :key="contacts.name">
+            <p v-if="numeros === contacts.numero">{{ contacts.name }}</p>
+          </div>
+          <p v-if="!isNumeroExist() && numeros">Inconnu</p>
         </div>
-        <div v-for="contacts in contact" :key="contacts.name">
-          <p v-if="numeros === contacts.numero">{{ contacts.name }}</p>
+      </form>
+      <div class="container_clavier">
+        <div class="clavier">
+          <button v-on:click="composer('1')">1</button>
+          <button v-on:click="composer('2')">2</button>
+          <button v-on:click="composer('3')">3</button>
+          <button v-on:click="composer('4')">4</button>
+          <button v-on:click="composer('5')">5</button>
+          <button v-on:click="composer('6')">6</button>
+          <button v-on:click="composer('7')">7</button>
+          <button v-on:click="composer('8')">8</button>
+          <button v-on:click="composer('9')">9</button>
+          <button class="call" @click="Call"><img src="../assets/call.png" alt="call_icon"></button>
+          <button v-on:click="composer('0')">0</button>
+          <button class="delete" @click="removeNum"><img src="../assets/larrow.png" alt="delete_icon"></button>
         </div>
-        <p v-if="!isNumeroExist() && numeros">Inconnu</p>
-      </div>
-    </form>
-    <div class="container_clavier">
-      <div class="clavier">
-        <button v-on:click="composer('1')">1</button>
-        <button v-on:click="composer('2')">2</button>
-        <button v-on:click="composer('3')">3</button>
-        <button v-on:click="composer('4')">4</button>
-        <button v-on:click="composer('5')">5</button>
-        <button v-on:click="composer('6')">6</button>
-        <button v-on:click="composer('7')">7</button>
-        <button v-on:click="composer('8')">8</button>
-        <button v-on:click="composer('9')">9</button>
-        <button class="call" @click="Call"><img src="../assets/call.png" alt="call_icon"></button>
-        <button v-on:click="composer('0')">0</button>
-        <button class="delete" @click="removeNum"><img src="../assets/larrow.png" alt="delete_icon"></button>
       </div>
     </div>
   </div>
+
 
 </template>
 
@@ -110,5 +113,19 @@ form {
   font-size: 12px;
   font-weight: bold;
   cursor: pointer;
+}
+
+.phone_container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 30px 0;
+}
+
+.phone {
+  border: 2px solid #fca311;
+  padding: 24px 0;
+  width: 300px;
+  border-radius: 24px
 }
 </style>
